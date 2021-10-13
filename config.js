3824c928a3aa9e504cf4d55aed9176679d4800a7
nieliangxu <yk8288196@126.com>
2021-10-13 20:45:08 +0800

    update

--- config.js
+++ config.js
@@ -74,13 +74,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开灯1",
+        turn_on: "开灯2",
         play: "音乐",
-        bannar_coming: "颜色",
+        // bannar_coming: "颜色",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
-        story: "一封给王晨小朋友的信",
+        story: "生日快乐，一封给王晨小朋友的信",
     }
 };
